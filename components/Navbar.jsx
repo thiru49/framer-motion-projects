ebfaf490c507aedfe8d09978698fbd10e6d0c3ff
thiru49 <thiruppathi497@gmail.com>
2024-01-01 22:23:03 +0530

    hero and navbar motion completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,25 @@
 'use client';
+import styles from '@/styles';
+import { navVariants } from '@/utils/motion';
+import {motion} from 'framer-motion'
+import Image from 'next/image';
 
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+  <motion.div 
+  variants={navVariants}
+  initial='hidden'
+  whileInView='show'
+  className={`${styles.xPaddings} py-8 relative`}>
+      <div className='absolute w-[50%] inset-0 gradient-01'/>
+      <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}> 
+         <Image src='/akar-icons_search.svg' alt='search' width={24} height={24} className='object-contain' />
+        <h2 className='font-eudoxus font-extrabold text-[24px] leading-[30px] text-white'>
+            METAVERSUS
+        </h2>
+        <Image src='/menu.svg' alt='menu' width={24} height={24} className='object-contain'/>  
+      </div>
+      
+  </motion.div>
 );
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './sections/**/*.{html,js,jsx}',
+    './styles/**/*.{js,jsx}',
   ],
   theme: {
     extend: {
